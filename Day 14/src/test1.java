5833717f73c290f7af4c93d0dd506a024ba24a9f
hellak1014 <108450086+hellak1014@users.noreply.github.com>
2022-07-19 18:04:03 +0900

    1

---
+++ Day 14/src/test1.java
@@ -0,0 +1,14 @@
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		
+		double base = 5.0;
+		double height = 2.0;
+				
+		double area = base * height / 2;
+		
+		System.out.println("area :" + area);
+		
+	}
+}
