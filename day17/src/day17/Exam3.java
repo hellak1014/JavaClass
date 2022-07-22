e2a0bde5f643f928051b6cbb20433da057fb9531
hellak1014 <108450086+hellak1014@users.noreply.github.com>
2022-07-22 18:17:33 +0900

    2
    2

---
+++ day17/src/day17/Exam3.java
@@ -0,0 +1,13 @@
+package day17;
+
+public class Exam3 {
+	public static void main(String[] args) {
+	
+		double[]a = new double[5];
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("a["+i+"]="+a[i]);
+		}
+		
+	}
+}
